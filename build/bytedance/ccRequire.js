8b239dc6692943b0236e9dce14b3319548a51b4a
wangZJ201205 <95744923+wangZJ201205@users.noreply.github.com>
2023-03-28 20:20:59 +0800

    更新
    更新1

---
+++ build/bytedance/ccRequire.js
@@ -0,0 +1,16 @@
+let moduleMap = {
+'assets/internal/index.js' () { return require('assets/internal/index.js') },
+'assets/resources/index.js' () { return require('assets/resources/index.js') },
+'assets/script/index.js' () { return require('assets/script/index.js') },
+'assets/startscene/index.js' () { return require('assets/startscene/index.js') },
+'src/scripts/main/index.js' () { return require('src/scripts/main/index.js') },
+// tail
+};
+
+window.__cocos_require__ = function (moduleName) {
+    let func = moduleMap[moduleName];
+    if (!func) {
+        throw new Error(`cannot find module ${moduleName}`);
+    }
+    return func();
+};
