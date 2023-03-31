157339e044c042e08125affd71e8b3df9793e648
wangZJ201205 <95744923+wangZJ201205@users.noreply.github.com>
2023-03-31 18:10:40 +0800

    消息
    消息

---
+++ Server/Common/WZJEvent.js
@@ -0,0 +1,41 @@
+/**
+ * 消息中心
+ */
+
+const WZJEvent = {
+    
+    _event : [],
+
+    On( eventname, callback, target) {
+        const message = {
+          id: this.messages.length + 1,
+          eventname,
+          callback,
+          target,
+        };
+        this._event.push(message);
+        return message.id;
+    },
+    
+    send( eventname, data ) {
+        for (let index = 0; index < this._event.length; index++) {
+            var event = this._event[index];
+            if(event.eventname == eventname)
+            {
+                event.callback( event.target, data );
+            }
+        }
+    },
+    
+    delete(eventId) {
+        const index = this._event.findIndex(message => message.id === eventId);
+        if (index !== -1) {
+          this._event.splice(index, 1);
+          return true;
+        }
+        return false;
+      },
+
+};
+
+wzj.event = module.exports = WZJEvent
--- Server/main.js
+++ Server/main.js
@@ -2,14 +2,16 @@
 //程序启动入口
 global.wzj = global.wzj || {};
 
-require('./net/WZJNetWork');
-require('./dbagent/WZJDBagent');
+require('./Common/WZJNetWork');
+require('./Common/WZJDBagent');
+require('./Common/WZJEvent');
 
 //初始化
 function init()
 {
     wzj.NetWork.init();
     wzj.dbagent.init();
+
 }
 
 //运行
@@ -17,6 +19,8 @@ function start()
 {
     wzj.NetWork.start();
     wzj.dbagent.start();
+    
+    
 }
 
 console.log('begin run server');
