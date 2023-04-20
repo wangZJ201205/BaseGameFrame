3ca9d3ffe1fa9067bab66734a05e48bf4346099e
wangZJ201205 <95744923+wangZJ201205@users.noreply.github.com>
2023-04-20 21:38:08 +0800

    仍然不对

--- Harmony/assets/script/ghost/StateMachine/States/EntityWalk.ts
+++ Harmony/assets/script/ghost/StateMachine/States/EntityWalk.ts
@@ -9,6 +9,11 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class EntityWalk extends StateParent {
 
+     // 当前对象的位置信息
+    protected currentPosition: cc.Vec3;
+
+    // 移动方向
+    protected direction: cc.Vec3;
 
     onLoad (id,host) 
     {
@@ -19,7 +24,13 @@ export default class EntityWalk extends StateParent {
     {
         console.info(">>>>>>>>>>EntityWalk:start");
         super.start();
+        var y = Math.random()* GameData.App_Game_Heigth ;
+        this.getHost().getEntityNode().setPosition(Math.random()*GameData.App_Game_Width  - GameData.App_Game_Width/2, y- GameData.App_Game_Heigth/2);
+        this.currentPosition = this.getHost().getEntityNode().position;
 
+        var heroNode = Hero.Instance.getEntity().getEntityNode();
+        this.direction = heroNode.position.sub(this.currentPosition);
+        this.direction.normalize();
     }
 
     stop()
@@ -32,16 +43,16 @@ export default class EntityWalk extends StateParent {
 
         var heroNode = Hero.Instance.getEntity().getEntityNode();
         var myNode = this.getHost().getEntityNode();
-        var currentPosition:cc.Vec3 = cc.v3(myNode.position.x,
-                                            myNode.position.y,0);
+        // var currentPosition:cc.Vec3 = cc.v3(myNode.position.x,
+        //                                     myNode.position.y,0);
 
-        var direction = heroNode.position.sub(currentPosition);
-        direction.normalize();
+        // var direction = heroNode.position.sub(currentPosition);
+        // direction.normalize();
         // entityNode.zIndex = GameData.App_Game_Heigth -(GameData.App_Game_Heigth/2 + entityNode.getPosition().y);
 
         
 
-        const distance = heroNode.position.sub(currentPosition).mag();
+        const distance = heroNode.position.sub(this.currentPosition).mag();
         console.info(">>>"+distance)
         if (distance <= 50) {
             // 到达目标节点
@@ -49,11 +60,12 @@ export default class EntityWalk extends StateParent {
             this.getHost().setClientProp(ClientDef.ENTITY_PROP_ACTIVE_STATE, ClientDef.ENTITY_ACTIVE_STATE_FREE);
           } else {
             // 沿着移动方向移动
-            const velocity = direction.mul(0.5 * dt);
-            // console.info(velocity.x,velocity.y,direction.x,direction.y,GameData.PayerMoveSpeed * 0.0005 * dt);
-            myNode.position = currentPosition.add(velocity);
+            const velocity = this.direction.mul(0.05 * dt);
+            console.info(velocity.x,velocity.y,GameData.PayerMoveSpeed * 0.0005 * dt);
+            this.currentPosition = this.currentPosition.add(velocity);
+            myNode.position = this.currentPosition.add(velocity);
           }
 
-        super.update(dt);
+        // super.update(dt);
     }
 }
